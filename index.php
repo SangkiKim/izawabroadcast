d8c208ca1f747b7e4366f62537a2dc98a5d2c1da
SangkiKim <45443722+SangkiKim@users.noreply.github.com>
2020-04-19 00:40:02 +0900

    add PhantomJS

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
 
-require_once("./phpQuery-onefile.php");
+require 'vendor/autoload.php';
 
-$contents = file_get_contents("https://tv.yahoo.co.jp/search/?q=IT");
-$html = count(phpQuery::newDocument($contents)->find('ul'));
-var_dump(phpQuery::newDocument($contents)->find('ul'));
-echo $html;
+    use JonnyW\PhantomJs\Client;
+    use JonnyW\PhantomJs\DependencyInjection\ServiceContainer;
+    
+    $client = Client::getInstance();
+    $client->getEngine()->setPath('bin\\phantomjs.exe');
+    /** 
+     * @see JonnyW\PhantomJs\Http\Request
+     **/
+    $request = $client->getMessageFactory()->createRequest('https://tv.yahoo.co.jp/search/?q=IT', 'GET');
 
+    /** 
+     * @see JonnyW\PhantomJs\Http\Response 
+     **/
+    $response = $client->getMessageFactory()->createResponse();
+
+    // Send the request
+    $client->send($request, $response);
+    var_dump($response);
+    if($response->getStatus() === 200) {
+
+        // Dump the requested page content
+        echo $response->getContent();
+    }
 ?>
