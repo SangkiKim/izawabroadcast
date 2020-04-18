173bd35911a58101174d435a3d87b3729fd5bd82
SangkiKim <45443722+SangkiKim@users.noreply.github.com>
2020-04-18 17:27:05 +0900

    Second commit

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Hello";
+
+require_once("./phpQuery-onefile.php");
+
+$contents = file_get_contents("https://tv.yahoo.co.jp/search/?q=IT");
+$html = count(phpQuery::newDocument($contents)->find('ul'));
+var_dump(phpQuery::newDocument($contents)->find('ul'));
+echo $html;
+
 ?>
